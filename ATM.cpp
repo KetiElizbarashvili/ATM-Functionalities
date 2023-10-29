dad9e00a3d6f6a1d7dd9544514845ab4093ad792
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-10-30 01:35:01 +0400

    getmobile

--- ATM.cpp
+++ ATM.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 
 
-class atm									
+class atm										// class atm 
 {
-private:										
+private:										// private member varibles
 	long int account_No;
 	string name;
 	int PIN;
 	double balance;
 	string mobile_No;
 
-public:											
+public:											// public member functions
 
-	
+	// setData function is setting the Data into the private member variables
 	void setData(long int account_No_a, string name_a, int PIN_a, double balance_a, string mobile_No_a)
 	{
-		account_No = account_No_a;	 
+		account_No = account_No_a;	 // assigning the formal arguments to the private member var's
 		name = name_a;
 		PIN = PIN_a;
 		balance = balance_a;
 		mobile_No = mobile_No_a;
 	}
 
-	
+	//getAccountNo function is returning the user's account no.
 	long int getAccountNo()
 	{
 		return account_No;
@@ -54,4 +54,5 @@ public:
 	{
 		return mobile_No;
 	}
- 
+
+	//setMobile function is Updating the user mobile no 
